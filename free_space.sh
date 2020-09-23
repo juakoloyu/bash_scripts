b4a786cf23ffafb89abb27be931ce9c4f18beee0
Joaquin Logiudice <juakoloyu@gmail.com>
2020-09-23 13:02:20 -0300

    Add comment

--- free_space.sh
+++ free_space.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #Script para enviar el espacio en disco del servidor
 #Desarrollado por Joaquin Logiudice
-# $1 = Host_to_post
+#Recibe como argumento $1 = Host a donde enviar el_post
 
 tamanio=$(df -h / | awk 'NR==2{ print $2}')
 usado=$(df -h / | awk 'NR==2{ print $3}')
 disponible=$(df -h / | awk 'NR==2{ print $4}')
-porcentaje_uso=$(df -h / | awk 'NR==2{ print $5}')
 host=$1
-echo $host
+ip=$(hostname -I | awk '{print $1}')
 nombre_servidor=$(hostname)
 detalle="Estado del espacio en disco"
 fecha=$(date '+%Y-%m-%d %H:%M')
-
+echo $ip
 $(curl --location --request POST "$host/servidores/Servidores/postServidores" \
 --form "tamanio=$tamanio" \
 --form "estado=$estado" \
 --form "usado=$usado" \
+--form "ip=$ip" \
 --form "disponible=$disponible" \
 --form "nombre_servidor=$nombre_servidor" \
 --form "fecha_servidor=$fecha" \
